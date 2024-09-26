30c88084d75c12c2cfbe268c499103fed83bb62c
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-09-25 21:31:58 -0500

    Nav bar done

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
+import CardDetails from './components/CardDetails.vue'
+import LoginPage from './components/LoginPage.vue' 
+import SettingsPage from './components/SettingsPage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/login', name: 'LoginPage', component: LoginPage},
+  { path: '/settings', name: 'SettingsPage', component: SettingsPage },
+     
 ]
 
 const router = createRouter({
