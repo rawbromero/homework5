2fb1691f97f29671ec40bb7645b5cf3f04f9928f
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-10-06 15:58:06 -0500

    pageination working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,12 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
   <MainCards></MainCards>
+  <MainPagination></MainPagination>
 
   
 </template>
